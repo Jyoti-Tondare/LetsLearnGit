8b46cbcba4662a50757d1c8a5067f5a0c594cae5
Jyoti Tondare <tondarejyoti85@gmail.com>
2023-08-14 12:41:33 +0530

    multiply.java is updated

--- multiply.java
+++ multiply.java
@@ -2,6 +2,7 @@ public class multiply {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
+        System.out.println("This is multiply file which is inside the dev branch");
         System.out.println("The multiplication is:" + (a * b));
     }
 }
