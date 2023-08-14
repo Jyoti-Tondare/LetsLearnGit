af580bdec39461995a8de5d45039338b4b2b6a99
Jyoti Tondare <tondarejyoti85@gmail.com>
2023-08-14 12:59:11 +0530

    Division added

---
+++ division.java
@@ -0,0 +1,8 @@
+public class division {
+    public static void main(String[] args) {
+        System.out.println("Here we will perform the division operation");
+        int a = 10;
+        int b = 20;
+        System.out.println("The division is:" + (b / a));
+    }
+}
